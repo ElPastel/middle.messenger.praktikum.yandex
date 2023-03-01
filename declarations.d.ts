4764c0cdb37f51508f5287a2c332cc8b26d02457
ElPastel <gromova.victoria9@gmail.com>
2023-03-01 16:21:15 +0300

    first pr

--- declarations.d.ts
+++ declarations.d.ts
@@ -3,21 +3,8 @@ declare module '*.pug' {
     export default _;
 }
 
-declare module '*.hbs' {
-    const _: Function
-    export default _;
-}
-
 declare module 'uuid' {
     type v4 = () => string;
     export const v4: v4;
 }
 
-// declare module 'pug' {
-//     type Pug = {
-//         [key: string]: any;
-//         render: ((template: string, props: Record<string, any>) => string)
-//     }
-//     const Templator: Pug
-//     export default Templator;
-// }
